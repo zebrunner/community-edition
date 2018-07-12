4fcb6b6ddb79231afdb01711522a35e4320a0153
vdelendik <vdelendik@qaprosoft.com>
2018-07-12 22:46:46 +0000

    added elastic search components and global ggr hub of hubs

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# to cauto clean selenoid/appium video crontab is configured to remove mp4 older than 24hrs
+#0 * * * * find $HOME/tools/stage-infra/selenoid/video -mindepth 1 -maxdepth 1 -mmin +1440 -name '*.mp4' | xargs rm -rf
+
 
 # pull required docker images
-docker pull selenoid/vnc:chrome_65.0
-docker pull selenoid/vnc:firefox_58.0
+docker pull selenoid/vnc:chrome_67.0
+docker pull selenoid/vnc:firefox_60.0
 docker pull selenoid/video-recorder
 
 #-------------- START EVERYTHING ------------------------------
