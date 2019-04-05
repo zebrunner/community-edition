cab773e09d058bc72ac8dafd4d056c9b5cca3ccd
vdelendik <vdelendik@qaprosoft.com>
2019-04-05 14:36:00 +0000

    custom host address setup closes #59

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd ${BASEDIR}
+
+HOST_NAME=$1
+
+if [ $# -lt 1 ]; then
+    printf 'Usage: %s HOST_NAME\n' "$(basename "$0")" >&2
+    exit -1
+fi
+
+sed 's/demo.qaprosoft.com/'$1'/g' variables.env.original > variables.env
+sed 's/demo.qaprosoft.com/'$1'/g' ./nginx/conf.d/default.conf.original > ./nginx/conf.d/default.conf
