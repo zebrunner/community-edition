55a5d7734eb5c92ce3f291c72aaf51a5f6026657
vdelendik <vdelendik@qaprosoft.com>
2020-02-10 11:47:29 +0000

    bump up to the 4.1.86 zafira

--- database/sql/db-app-state-management.sql
+++ database/sql/db-app-state-management.sql
@@ -137,4 +137,12 @@ INSERT INTO databasechangelog(
      current_timestamp,
      1,
      'EXECUTED'
+ ),
+ (
+     '014_add_launcher_presets_launchers_on_delete',
+     'brutskov',
+     'classpath:db/changelog.yml',
+     current_timestamp,
+     1,
+     'EXECUTED'
  );
