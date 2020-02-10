ad4ce4361de0338ad319f600f24f5cecc0927574
ilya-varchenya <ilya.varchenya@gmail.com>
2020-02-10 11:39:26 +0300

    update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 docker-compose stop
+
+if [ "$1" == "--keep-containers" ]; then
+    echo "Containers were kept"
+else
+    echo "Containers were deleted"
+    docker-compose rm -fv
+fi
