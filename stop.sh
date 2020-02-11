551337f12d9ab806a457bcc7b0492f590f2aac36
ilya-varchenya <ilya.varchenya@gmail.com>
2020-02-11 13:50:40 +0300

    return to using flag

--- stop.sh
+++ stop.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-flag=true
+
+keep_containers = true
+
 for arg in "$@"
 do
   case $arg in
     "--help" | "-h")
       echo "
       Flags:\n
-      --keep-containers, -k    keep containers\n
+      --remove, -rm    remove containers\n
       For more help visit telegram chanal https://t.me/qps_infra"
       ;;
 
-    "--keep-containers" | "-k")
-      echo "Containers were kept"
-      flag=false
+    "--remove" | "-rm")
+      $keep_containers = false
       ;;
-  *)
-    echo "There's no such parameter" + $arg
-    ;;
+    *)
+      echo "There's no such parameter" + $arg
+      ;;
 esac
 done
 
 docker-compose stop
 echo "qps-infra was stopped"
 
-if $flag; then
+if [ !$keep_containers ]; then
   docker-compose rm -fv
   echo "Containers were deleted"
 fi
