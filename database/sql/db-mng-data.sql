a15a61f43a005e023e70609264621dc733df4c4d
vdelendik <vdelendik@qaprosoft.com>
2020-02-27 12:35:50 +0000

    hotfixed to the zafira schema/widgets creation

--- database/sql/db-mng-data.sql
+++ database/sql/db-mng-data.sql
@@ -1606,7 +1606,8 @@ chart.setOption(option);', '{
 }', false);
 
 
-INSERT INTO WIDGET_TEMPLATES (NAME, DESCRIPTION, TYPE, SQL, CHART_CONFIG, PARAMS_CONFIG, LEGEND_CONFIG, MODIFIED_AT, CREATED_AT, PARAMS_CONFIG_SAMPLE, HIDDEN) VALUES ('PASS RATE (PIE)', 'Consolidated test status information with the ability to specify 10+ extra filters including daily, weekly, monthly, etc period.', 'PIE', '<#global IGNORE_PERSONAL_PARAMS = ["OWNER_USERNAME"] >
+INSERT INTO WIDGET_TEMPLATES (NAME, DESCRIPTION, TYPE, SQL, CHART_CONFIG, PARAMS_CONFIG, LEGEND_CONFIG, MODIFIED_AT, CREATED_AT, PARAMS_CONFIG_SAMPLE, HIDDEN) VALUES ('PASS RATE (PIE)', 'Consolidated test status information with the ability to specify 10+ extra filters including daily, weekly, monthly, etc period.', 'PIE', '<#global IGNORE_TOTAL_PARAMS = ["DEVICE", "APP_VERSION", "LOCALE", "LANGUAGE", "JOB_NAME", "PARENT_JOB", "PARENT_BUILD"] > 
+<#global IGNORE_PERSONAL_PARAMS = ["OWNER_USERNAME"] >
 
 <#global MULTIPLE_VALUES = {
   "PROJECT": multiJoin(PROJECT, projects),
@@ -1678,6 +1679,10 @@ SELECT
  <#local result = "" />
  <#list map?keys as key>
     <#if map[key] != "" >
+      <#if PERIOD == "Total" && IGNORE_TOTAL_PARAMS?seq_contains(key)>
+        <#-- Ignore non supported filters for Total View: PLATFORM, DEVICE, APP_VERSION, LOCALE, LANGUAGE, JOB_NAME-->
+        <#continue>
+      </#if>
       <#if PERSONAL == "true" && IGNORE_PERSONAL_PARAMS?seq_contains(key)>
         <#-- Ignore non supported filters for Personal chart: USER -->
         <#continue>
@@ -1689,6 +1694,7 @@ SELECT
     </#if>
  </#list>
 
+<#if PERIOD != "Total">
  <#if PARENT_JOB != "">
     <#if result?length != 0>
       <#local result = result + " AND "/>
@@ -1711,6 +1717,7 @@ SELECT
     </#if>
     <#local result = result + "UPSTREAM_JOB_BUILD_NUMBER = ''" + PARENT_BUILD + "''"/>  
   </#if>
+</#if>
 
  <#if result?length != 0 && PERSONAL == "true">
    <!-- add personal filter by currentUserId with AND -->
@@ -1755,6 +1762,9 @@ SELECT
     <#break>
   <#case "Monthly">
     <#local result = "MONTHLY_VIEW" />
+    <#break>
+  <#case "Total">
+    <#local result = "TOTAL_VIEW" />
     <#break>
  </#switch>
  <#return result>
@@ -1849,7 +1859,8 @@ chart.setOption(option);', '{
       "Last 30 Days",
       "Nightly",
       "Weekly",
-      "Monthly"
+      "Monthly",
+      "Total"
       ],
     "required": true
   },
@@ -2281,7 +2292,7 @@ SELECT count(*) as "COUNT",
  </#switch>
  <#return result>
 </#function>
-', '{"legend": ["ENV", "REPORT", "BUG", "SUBJECT"]}', '{
+', '{"columns": ["ENV", "REPORT", "BUG", "SUBJECT"]}', '{
   "PERIOD": {
     "values": [
       "Last 24 Hours",
@@ -3015,7 +3026,8 @@ SELECT
 
 INSERT INTO WIDGET_TEMPLATES (NAME, DESCRIPTION, TYPE, SQL, CHART_CONFIG, PARAMS_CONFIG, LEGEND_CONFIG, MODIFIED_AT, CREATED_AT, PARAMS_CONFIG_SAMPLE, HIDDEN) VALUES ('TEST CASES BY STABILITY', 'Shows all test cases with low stability percent rate per appropriate period (default - less than 10%).', 'TABLE', '
 <#-- TODO: remove PARENT_JOB from IGNORE_TOTAL_PARAMS as only TEST_CASE_HEALTH_VIEW could have PARENT_JOB/UPSTREAM_JOB_NAME argument-->
-<#global IGNORE_TOTAL_PARAMS = ["LOWER(PLATFORM)", "OWNER_USERNAME", "ENV", "PRIORITY", "FEATURE", "TASK", "DEVICE", "APP_VERSION", "LOCALE", "LANGUAGE", "PARENT_JOB", "JOB_NAME"] >
+<#-- ZEB-703 Widgets:For widget TEST CASES BY STABILITY with param BUG appears sql error -->
+<#global IGNORE_TOTAL_PARAMS = ["LOWER(PLATFORM)", "OWNER_USERNAME", "ENV", "PRIORITY", "FEATURE", "TASK", "BUG", "BROWSER", "DEVICE", "APP_VERSION", "LOCALE", "LANGUAGE", "PARENT_JOB", "JOB_NAME"] >
 
 <#global MULTIPLE_VALUES = {
   "LOWER(PLATFORM)": join(PLATFORM),
@@ -3079,20 +3091,21 @@ INSERT INTO WIDGET_TEMPLATES (NAME, DESCRIPTION, TYPE, SQL, CHART_CONFIG, PARAMS
       <#-- Ignore non supported filters for Total View: PLATFORM, DEVICE, APP_VERSION, LOCALE, LANGUAGE, JOB_NAME-->
         <#continue>
       </#if>
+
       <#if result?length != 0>
        <#local result = result + " AND "/>
       </#if>
-
-      <#if PARENT_JOB != "">
-        <#if result?length != 0>
-          <#local result = result + " AND "/>
-        </#if>
-        <#local result = result + "UPSTREAM_JOB_NAME = ''" + PARENT_JOB + "''"/>
-      </#if>   
-
       <#local result = result + key + " LIKE ANY (''{" + map[key] + "}'')"/>
     </#if>
   </#list>
+
+
+  <#if PARENT_JOB != "" && PERIOD != "Total" && PERIOD != "Monthly">
+    <#if result?length != 0>
+      <#local result = result + " AND "/>
+    </#if>
+    <#local result = result + "UPSTREAM_JOB_NAME = ''" + PARENT_JOB + "''"/>
+  </#if>  
 
  <#if result?length != 0>
   <#local result = " WHERE " + result/>
