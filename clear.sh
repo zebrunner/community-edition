4be49d9ae6dcec86631f390dc128226c66ac5d44
vdelendik <vdelendik@qaprosoft.com>
2020-04-28 11:28:14 +0000

    improved clear.sh using docker-compose down -v

--- clear.sh
+++ clear.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
-docker-compose stop
-docker-compose rm -f
+docker-compose -f jenkins/docker-compose.yml down -v
+docker-compose -f reporting-service/docker-compose.yml down -v
+docker-compose -f sonarqube/docker-compose.yml down -v
 
 docker rm -fv $(docker ps -a -q)
+#TODO: make images removal only for infra images somehow!
 docker rmi -f $(docker images -q)
-
-# remove sonarqube data
-docker volume rm sonarqube_data-volume sonarqube_extensions-volume sonarqube_logs-volume
-
-#remove jenkins data
-docker volume rm jenkins_data-volume
-
-# remove reporting-service data
-docker volume rm reporting-service_assets-data reporting-service_esdata-volume reporting-service_pgdata-volume
 
 rm -rf ./selenoid/video/*
 
