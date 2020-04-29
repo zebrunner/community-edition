0c81977e80936070705b6814c1326ac7871cd2a7
vdelendik <vdelendik@qaprosoft.com>
2020-04-29 23:17:17 +0000

    added stf as part of MCloud solution

--- clear.sh
+++ clear.sh
@@ -3,6 +3,7 @@
 docker-compose -f jenkins/docker-compose.yml down -v
 docker-compose -f reporting-service/docker-compose.yml down -v
 docker-compose -f sonarqube/docker-compose.yml down -v
+docker-compose -f mcloud/docker-compose.yml down -v
 docker-compose docker-compose.yml down -v
 
 docker rm -fv $(docker ps -a -q)
