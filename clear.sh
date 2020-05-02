d3d3aebba55ca9155c91348c5ce181f6a883b41a
vdelendik <vdelendik@solvd.com>
2020-05-02 17:25:32 +0000

    more changes to setup and clear process

--- clear.sh
+++ clear.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${BASEDIR}
 
 docker-compose -f jenkins/docker-compose.yml down -v
 docker-compose -f reporting-service/docker-compose.yml down -v
@@ -13,5 +16,8 @@ docker rmi -f $(docker images -q)
 
 rm -rf ./selenoid/video/*.mp4
 mv selenoid/browsers.json selenoid/browsers.json.bak
+mv variables.env variables.env.bak
+mv ./nginx/conf.d/default.conf ./nginx/conf.d/default.conf.bak
+mv .env .env.bak
 
 
--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
 cd ${BASEDIR}
 
 HOST_NAME=$1
@@ -12,6 +11,7 @@ if [ $# -lt 1 ]; then
 fi
 
 echo generating variables.env...
+sed 's/demo.qaprosoft.com/'$1'/g' .env.original > .env
 sed 's/demo.qaprosoft.com/'$1'/g' variables.env.original > variables.env
 echo generating ./nginx/conf.d/default.conf...
 sed 's/demo.qaprosoft.com/'$1'/g' ./nginx/conf.d/default.conf.original > ./nginx/conf.d/default.conf
