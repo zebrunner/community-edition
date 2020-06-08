4c674fbf3294fab7572e3ead6f73f890cfc5aede
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-08 16:04:42 +0300

    update todo

--- selenoid/update.sh
+++ selenoid/update.sh
@@ -3,7 +3,7 @@
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${BASEDIR}
 
-#TODO: execute binary based on host architecture to support mac/win as well
+#TODO: execute binary based on host architecture to support win as well
 
 echo downloading latest chrome/firefox/opera browser images
 ${BASEDIR}/bin/bash selenoid update --vnc --config-dir "${BASEDIR}" $*
