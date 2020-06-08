0c63c93d6ecabb43b8755c12a956b4d21fff1b3f
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-08 15:59:28 +0300

    update binary

--- selenoid/update.sh
+++ selenoid/update.sh
@@ -6,6 +6,6 @@ cd ${BASEDIR}
 #TODO: execute binary based on host architecture to support mac/win as well
 
 echo downloading latest chrome/firefox/opera browser images
-${BASEDIR}/bin/cm_linux_amd64 selenoid update --vnc --config-dir "${BASEDIR}" $*
+${BASEDIR}/bin/bash selenoid update --vnc --config-dir "${BASEDIR}" $*
 
 docker rm -f selenoid
