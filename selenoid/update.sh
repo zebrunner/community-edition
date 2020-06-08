a7e84d9288cf7a11573a93f7cef3208985d36b5d
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-08 16:38:27 +0300

    return bash script for avoiding aerokube add

--- selenoid/update.sh
+++ selenoid/update.sh
@@ -6,8 +6,7 @@ cd ${BASEDIR}
 #TODO: execute binary based on host architecture to support win as well
 
 echo downloading latest chrome/firefox/opera browser images
-curl -s https://aerokube.com/cm/bash
-./cm selenoid update --vnc --config-dir "${BASEDIR}" $*
+${BASEDIR}/bin/bash selenoid update --vnc --config-dir "${BASEDIR}" $*
 
 if [ ! "docker ps -q -f status=running -f name=selenoid" ]; then
     docker rm -f selenoid
