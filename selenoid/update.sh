f9d6e3ad4b9d0f8ea44f6bb6c716cdebc0f2fa55
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-10 10:50:58 +0300

    edit update.sh

--- selenoid/update.sh
+++ selenoid/update.sh
@@ -6,9 +6,7 @@ cd ${BASEDIR}
 #TODO: execute binary based on host architecture to support win as well
 
 echo downloading latest chrome/firefox/opera browser images
-${BASEDIR}/bin/download.sh selenoid start --vnc
-${BASEDIR}/bin/download.sh selenoid update --vnc --config-dir "${BASEDIR}" $*
+${BASEDIR}download.sh selenoid start --vnc
+${BASEDIR}/cm selenoid update --vnc --config-dir "${BASEDIR}" $*
 
-if [ ! "docker ps -q -f status=running -f name=selenoid" ]; then
-    docker rm -f selenoid
-fi
+docker rm -f selenoid
