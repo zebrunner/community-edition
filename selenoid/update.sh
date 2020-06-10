f528fd881b91eeb43b89ea5f7260ff784cbb5255
vdelendik <vadim.delendik@gmail.com>
2020-06-10 14:42:25 +0300

    Revert "create config manager for mac"

--- selenoid/update.sh
+++ selenoid/update.sh
@@ -3,8 +3,9 @@
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${BASEDIR}
 
+#TODO: execute binary based on host architecture to support mac/win as well
+
 echo downloading latest chrome/firefox/opera browser images
-${BASEDIR}/download.sh
-${BASEDIR}/bin/cm selenoid update --vnc --config-dir "${BASEDIR}" $*
+${BASEDIR}/bin/cm_linux_amd64 selenoid update --vnc --config-dir "${BASEDIR}" $*
 
 docker rm -f selenoid
