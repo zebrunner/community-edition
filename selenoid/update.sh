1251fbafedc913aba35ecef70f69aa196c1043b6
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-10 10:54:54 +0300

    hotfix

--- selenoid/update.sh
+++ selenoid/update.sh
@@ -6,7 +6,7 @@ cd ${BASEDIR}
 #TODO: execute binary based on host architecture to support win as well
 
 echo downloading latest chrome/firefox/opera browser images
-${BASEDIR}download.sh selenoid start --vnc
+${BASEDIR}/download.sh selenoid start --vnc
 ${BASEDIR}/cm selenoid update --vnc --config-dir "${BASEDIR}" $*
 
 docker rm -f selenoid
