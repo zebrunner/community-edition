a0adbfe3dd5d22e152e41497bdad45939effbe3f
Ubuntu <ubuntu@ip-172-31-12-84.ec2.internal>
2020-08-05 22:26:37 +0000

    generated common administration shell script: zebrunner.sh

--- start.sh
+++ start.sh
@@ -1,41 +1,7 @@
 #!/bin/bash
 
-# to cauto clean selenoid/appium video crontab is configured to remove mp4 older than 24hrs
-#0 * * * * find $HOME/tools/stage-infra/selenoid/video -mindepth 1 -maxdepth 1 -mmin +1440 -name '*.mp4' | xargs rm -rf
-
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd ${BASEDIR}
+./zebrunner.sh start
 
-if [ ! -f .env ] || [ ! -f ./nginx/conf.d/default.conf ] || [ ! -f ./selenoid/browsers.json ]; then
-    printf 'WARNING! You forgot to setup infra host address preliminary! For example:\n ./setup.sh myhost.domain.com\n\n' "$(basename "$0")" >&2
-    exit -1
-fi
-
-echo "
-███████╗███████╗██████╗ ██████╗ ██╗   ██╗███╗   ██╗███╗   ██╗███████╗██████╗
-╚══███╔╝██╔════╝██╔══██╗██╔══██╗██║   ██║████╗  ██║████╗  ██║██╔════╝██╔══██╗
-  ███╔╝ █████╗  ██████╔╝██████╔╝██║   ██║██╔██╗ ██║██╔██╗ ██║█████╗  ██████╔╝
- ███╔╝  ██╔══╝  ██╔══██╗██╔══██╗██║   ██║██║╚██╗██║██║╚██╗██║██╔══╝  ██╔══██╗
-███████╗███████╗██████╔╝██║  ██║╚██████╔╝██║ ╚████║██║ ╚████║███████╗██║  ██║
-╚══════╝╚══════╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
-
-        ███████╗████████╗ █████╗ ██████╗ ████████╗███████╗██████╗
-        ██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗
-        ███████╗   ██║   ███████║██████╔╝   ██║   █████╗  ██████╔╝
-        ╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ██╔══╝  ██╔══██╗
-        ███████║   ██║   ██║  ██║██║  ██║   ██║   ███████╗██║  ██║
-        ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
-
-"
-
-# create infra network only if not exist
-docker network inspect infra >/dev/null 2>&1 || docker network create infra
-
-#-------------- START EVERYTHING ------------------------------
-docker-compose -f selenoid/docker-compose.yml up -d
-docker-compose -f mcloud/docker-compose.yml up -d
-docker-compose -f jenkins/docker-compose.yml up -d
-docker-compose -f reporting-service/docker-compose.yml up -d
-docker-compose -f sonarqube/docker-compose.yml up -d
-docker-compose up -d
--- stop.sh
+++ stop.sh
@@ -1,65 +1,6 @@
 #!/bin/bash
 
-KEEP_CONTAINERS=false
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-echo_help() {
-  echo "
-      Flags:
-          --keep-containers | -k    keep containers
-      For more help join telegram channel https://t.me/qps_infra"
-      exit 0
-  }
-
-echo "
-███████╗███████╗██████╗ ██████╗ ██╗   ██╗███╗   ██╗███╗   ██╗███████╗██████╗
-╚══███╔╝██╔════╝██╔══██╗██╔══██╗██║   ██║████╗  ██║████╗  ██║██╔════╝██╔══██╗
-  ███╔╝ █████╗  ██████╔╝██████╔╝██║   ██║██╔██╗ ██║██╔██╗ ██║█████╗  ██████╔╝
- ███╔╝  ██╔══╝  ██╔══██╗██╔══██╗██║   ██║██║╚██╗██║██║╚██╗██║██╔══╝  ██╔══██╗
-███████╗███████╗██████╔╝██║  ██║╚██████╔╝██║ ╚████║██║ ╚████║███████╗██║  ██║
-╚══════╝╚══════╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
-
-        ███████╗████████╗ █████╗ ██████╗ ████████╗███████╗██████╗
-        ██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗
-        ███████╗   ██║   ███████║██████╔╝   ██║   █████╗  ██████╔╝
-        ╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ██╔══╝  ██╔══██╗
-        ███████║   ██║   ██║  ██║██║  ██║   ██║   ███████╗██║  ██║
-        ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
-
-"
-
-for arg in "$@"
-do
-  case $arg in
-    --help | -h)
-      echo_help;
-      shift
-      ;;
-    --keep-containers | -k)
-      KEEP_CONTAINERS=true;
-      shift
-      ;;
-    *)
-      echo "There's no such parameter" $arg
-      echo_help
-      ;;
-esac
-done
-
-if $KEEP_CONTAINERS
-then
-  docker-compose -f jenkins/docker-compose.yml stop
-  docker-compose -f reporting-service/docker-compose.yml stop
-  docker-compose -f sonarqube/docker-compose.yml stop
-  docker-compose -f mcloud/docker-compose.yml stop
-  docker-compose -f selenoid/docker-compose.yml stop
-  docker-compose stop
-  echo "Containers were stopped"
-else 
-  docker-compose -f jenkins/docker-compose.yml down
-  docker-compose -f reporting-service/docker-compose.yml down
-  docker-compose -f sonarqube/docker-compose.yml down
-  docker-compose -f mcloud/docker-compose.yml down
-  docker-compose -f selenoid/docker-compose.yml down
-  docker-compose down
-  echo "Containers were stopped and deleted"
-fi
+cd ${BASEDIR}
+./zebrunner.sh stop
