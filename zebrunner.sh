7c0229d1a092c7877e797cd2c8a273fe090e985f
vdelendik <vdelendik@zebrunner.com>
2020-08-17 17:40:03 +0000

    parametrize reporting settings: postgres creds, rabbitmq, redis, smtp etc

--- zebrunner.sh
+++ zebrunner.sh
@@ -15,6 +15,9 @@
   setup() {
     print_banner
 
+    # load default interactive installer settings
+    source backup/settings.env.original
+
     # load ./backup/settings.env if exist to declare ZBR* vars from previous run!
     if [[ -f backup/settings.env ]]; then
       source backup/settings.env
@@ -31,6 +34,9 @@
     if [[ $ZBR_REPORTING_ENABLED -eq 1 ]]; then
       enableLayer "reporting/minio-storage" "Enable Zebrunner Minio Storage for Reporting?"
       ZBR_MINIO_ENABLED=$?
+      set_reporting_settings
+
+      set_storage_settings
     else
       # no need to ask about enabling minio sub-module
       disableLayer "reporting/minio-storage"
@@ -177,14 +183,8 @@
   set_global_settings() {
     # Setup global settings: protocol, hostname and port
     local is_confirmed=0
-    if [[ -z $ZBR_PROTOCOL ]]; then
-      ZBR_PROTOCOL=http
-    fi
     if [[ -z $ZBR_HOSTNAME ]]; then
       ZBR_HOSTNAME=$HOSTNAME
-    fi
-    if [[ -z $ZBR_PORT ]]; then
-      ZBR_PORT=80
     fi
 
     while [[ $is_confirmed -eq 0 ]]; do
@@ -203,7 +203,9 @@
         ZBR_PORT=$local_port
       fi
 
-      confirm "URL: $ZBR_PROTOCOL://$ZBR_HOSTNAME:$ZBR_PORT" "Continue?"
+      echo 
+      echo "URL:"
+      confirm "$ZBR_PROTOCOL://$ZBR_HOSTNAME:$ZBR_PORT" "Continue?"
       is_confirmed=$?
     done
 
@@ -213,8 +215,179 @@
 
   }
 
+  set_reporting_settings() {
+    # Collect reporting settings
+    ## Crypto token and salt
+    local is_confirmed=0
+    while [[ $is_confirmed -eq 0 ]]; do
+      read -p "SIGNIN TOKEN SECRET (randomized base64 encoded string) [$ZBR_TOKEN_SIGNING_SECRET]: " local_token
+      if [[ ! -z $local_token ]]; then
+        ZBR_TOKEN_SIGNING_SECRET=$local_token
+      fi
+
+      read -p "CRYPTO SALT [$ZBR_CRYPTO_SALT]: " local_salt
+      if [[ ! -z $local_salt ]]; then
+        ZBR_CRYPTO_SALT=$local_salt
+      fi
+
+      echo
+      echo "REPORTING CRYPTO:"
+      echo "SIGNIN TOKEN SECRET=$ZBR_TOKEN_SIGNING_SECRET"
+      echo "CRYPTO SALT=$ZBR_CRYPTO_SALT"
+      confirm "Reporting Crypto Settings" "Continue?"
+      is_confirmed=$?
+    done
+
+    export ZBR_TOKEN_SIGNING_SECRET=$ZBR_TOKEN_SIGNING_SECRET
+    export ZBR_CRYPTO_SALT=$ZBR_CRYPTO_SALT
+
+
+    ## iam-service posgtres
+    local is_confirmed=0
+    while [[ $is_confirmed -eq 0 ]]; do
+      read -p "IAM POSTGRES USER [$ZBR_IAM_POSTGRES_USER]: " local_iam_postgres_user
+      if [[ ! -z $local_iam_postgres_user ]]; then
+        ZBR_IAM_POSTGRES_USER=$local_iam_postgres_user
+      fi
+
+      read -p "IAM POSTGRES PASSWORD [$ZBR_IAM_POSTGRES_PASSWORD]: " local_iam_postgres_password
+      if [[ ! -z $local_iam_postgres_password ]]; then
+        ZBR_IAM_POSTGRES_PASSWORD=$local_iam_postgres_password
+      fi
+
+      echo
+      echo "REPORTING IAM POSTGRES:"
+      echo "USER=$ZBR_IAM_POSTGRES_USER"
+      echo "PASSWORD=$ZBR_IAM_POSTGRES_PASSWORD"
+      confirm "Reporting IAM Postgres Credentials" "Continue?"
+      is_confirmed=$?
+    done
+
+    export ZBR_IAM_POSTGRES_USER=$ZBR_IAM_POSTGRES_USER
+    export ZBR_IAM_POSTGRES_PASSWORD=$ZBR_IAM_POSTGRES_PASSWORD
+
+
+    ## reporting posgtres instance
+    local is_confirmed=0
+    while [[ $is_confirmed -eq 0 ]]; do
+      read -p "POSTGRES USER [$ZBR_POSTGRES_USER]: " local_postgres_user
+      if [[ ! -z $local_postgres_user ]]; then
+        ZBR_POSTGRES_USER=$local_postgres_user
+      fi
+
+      read -p "POSTGRES PASSWORD [$ZBR_POSTGRES_PASSWORD]: " local_postgres_password
+      if [[ ! -z $local_postgres_password ]]; then
+        ZBR_POSTGRES_PASSWORD=$local_postgres_password
+      fi
+
+      echo
+      echo "REPORTING POSTGRES:"
+      echo "USER=$ZBR_POSTGRES_USER"
+      echo "PASSWORD=$ZBR_POSTGRES_PASSWORD"
+      confirm "Reporting Postgres Credentials" "Continue?"
+      is_confirmed=$?
+    done
+
+    export ZBR_POSTGRES_USER=$ZBR_POSTGRES_USER
+    export ZBR_POSTGRES_PASSWORD=$ZBR_POSTGRES_PASSWORD
+
+
+    ## email-service (smtp)
+    local is_confirmed=0
+    while [[ $is_confirmed -eq 0 ]]; do
+      read -p "SMTP HOST [$ZBR_SMTP_HOST]: " local_smtp_host
+      if [[ ! -z $local_smtp_host ]]; then
+        ZBR_SMTP_HOST=$local_smtp_host
+      fi
+
+      read -p "SMTP PORT [$ZBR_SMTP_PORT]: " local_smtp_port
+      if [[ ! -z $local_smtp_port ]]; then
+        ZBR_SMTP_PORT=$local_smtp_port
+      fi
+
+      read -p "SMTP USER EMAIL[$ZBR_SMTP_EMAIL]: " local_smtp_email
+      if [[ ! -z $local_smtp_email ]]; then
+        ZBR_SMTP_EMAIL=$local_smtp_email
+      fi
+
+      read -p "SMTP USER [$ZBR_SMTP_USER]: " local_smtp_user
+      if [[ ! -z $local_smtp_user ]]; then
+        ZBR_SMTP_USER=$local_smtp_user
+      fi
+
+      read -p "SMTP PASSWORD [$ZBR_SMTP_PASSWORD]: " local_smtp_password
+      if [[ ! -z $local_smtp_password ]]; then
+        ZBR_SMTP_PASSWORD=$local_smtp_password
+      fi
+
+      echo
+      echo "SMTP EMAIL SETTINGS:"
+      echo "HOST:PORT=$ZBR_SMTP_HOST:$ZBR_SMTP_PORT"
+      echo "SENDER EMAIL=$ZBR_SMTP_EMAIL"
+      echo "USER/PASSWORD=$ZBR_SMTP_USER/$ZBR_SMTP_PASSWORD"
+      confirm "Reporting Email (SMTP) Credentials" "Continue?"
+      is_confirmed=$?
+    done
+
+    export ZBR_SMTP_HOST=$ZBR_SMTP_HOST
+    export ZBR_SMTP_PORT=$ZBR_SMTP_PORT
+    export ZBR_SMTP_EMAIL=$ZBR_SMTP_EMAIL
+    export ZBR_SMTP_USER=$ZBR_SMTP_USER
+    export ZBR_SMTP_PASSWORD=$ZBR_SMTP_PASSWORD
+
+
+    ## reporting rabbitmq
+    local is_confirmed=0
+    while [[ $is_confirmed -eq 0 ]]; do
+      read -p "RABBITMQ USER [$ZBR_RABBITMQ_USER]: " local_rabbitmq_user
+      if [[ ! -z $local_rabbitmq_user ]]; then
+        ZBR_RABBITMQ_USER=$local_rabbitmq_user
+      fi
+
+      read -p "RABBITMQ PASSWORD [$ZBR_RABBITMQ_PASSWORD]: " local_rabbitmq_password
+      if [[ ! -z $local_rabbitmq_password ]]; then
+        ZBR_RABBITMQ_PASSWORD=$local_rabbitmq_password
+      fi
+
+      echo
+      echo "REPORTING RABBITMQ:"
+      echo "USER/PASSWORD=$ZBR_RABBITMQ_USER/$ZBR_RABBITMQ_PASSWORD"
+      confirm "Reporting Rabbitmq Credentials" "Continue?"
+      is_confirmed=$?
+    done
+
+    export ZBR_RABBITMQ_USER=$ZBR_RABBITMQ_USER
+    export ZBR_RABBITMQ_PASSWORD=$ZBR_RABBITMQ_PASSWORD
+
+    ## reporting redis
+    local is_confirmed=0
+    while [[ $is_confirmed -eq 0 ]]; do
+      read -p "REDIS PASSWORD [$ZBR_REDIS_PASSWORD]: " local_redis_password
+      if [[ ! -z $local_redis_password ]]; then
+        ZBR_REDIS_PASSWORD=$local_redis_password
+      fi
+
+      echo
+      echo "REPORTING REDIS:"
+      echo "PASSWORD=$ZBR_REDIS_PASSWORD"
+      confirm "Reporting Redis Credentials" "Continue?"
+      is_confirmed=$?
+    done
+
+    export ZBR_REDIS_PASSWORD=$ZBR_REDIS_PASSWORD
+  }
+
+  set_storage_settings() {
+    ## Minio or AWS S3 storage 
+    echo "TODO: implement storage minio creds setup"
+  }
+
   export_settings() {
     export -p | grep "ZBR" > backup/settings.env
+  }
+
+  random_string() {
+    cat /dev/urandom | env LC_CTYPE=C tr -dc a-zA-Z0-9 | head -c 48; echo
   }
 
   confirm() {
