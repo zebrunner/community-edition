28e7f84b7ae60bfe48b5a0830709d203430466d0
vdelendik <vdelendik@zebrunner.com>
2020-09-30 15:37:48 +0000

    apply configuration changes for reporting

--- patch/1.1.sh
+++ patch/1.1.sh
@@ -21,6 +21,24 @@ fi
 
 echo "Upgrading Zebrunner from ${SOURCE_VERSION} to ${TARGET_VERSION}"
 # Zebrunner Reporting steps:
+
+# apply new nginx rules for logs and screenshots artifacts
+cp reporting/configuration/zebrunner-proxy/nginx.conf.original reporting/configuration/zebrunner-proxy/nginx.conf
+if [[ $ZBR_MINIO_ENABLED -eq 0 ]]; then
+  # use case with AWS S3
+  sed -i "s#custom_secret_value#${ZBR_STORAGE_AGENT_KEY}#g" reporting/configuration/zebrunner-proxy/nginx.conf
+  sed -i "s#/zebrunner/#/${ZBR_STORAGE_BUCKET}/#g" reporting/configuration/zebrunner-proxy/nginx.conf
+  sed -i "s#http://minio:9000#${ZBR_STORAGE_ENDPOINT_PROTOCOL}://${ZBR_STORAGE_ENDPOINT_HOST}#g" reporting/configuration/zebrunner-proxy/nginx.conf
+fi
+
+# apply new elasticsearch variables
+cp reporting/configuration/reporting-service/variables.env.original reporting/configuration/reporting-service/variables.env
+sed -i "s#http://localhost:8081#${url}#g" reporting/configuration/reporting-service/variables.env
+
+sed -i "s#GITHUB_HOST=github.com#GITHUB_HOST=${ZBR_GITHUB_HOST}#g" reporting/configuration/reporting-service/variables.env
+sed -i "s#GITHUB_CLIENT_ID=#GITHUB_CLIENT_ID=${ZBR_GITHUB_CLIENT_ID}#g" reporting/configuration/reporting-service/variables.env
+sed -i "s#GITHUB_CLIENT_SECRET=#GITHUB_CLIENT_SECRET=${ZBR_GITHUB_CLIENT_SECRET}#g" reporting/configuration/reporting-service/variables.env
+
 # apply new logstash settings 
 cp reporting/configuration/logstash/logstash.conf.original reporting/configuration/logstash/logstash.conf
 sed -i "s#rabbitmq-user#${ZBR_RABBITMQ_USER}#g" reporting/configuration/logstash/logstash.conf
