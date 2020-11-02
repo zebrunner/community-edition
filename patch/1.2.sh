a5f0f031c8bc11696111fdacfba31b6b8f501e07
vdelendik <vdelendik@zebrunner.com>
2020-11-02 20:47:25 +0000

    #337 and #339: override if possible global choice lists and libraries

--- patch/1.2.sh
+++ patch/1.2.sh
@@ -1,9 +1,48 @@
 #!/bin/bash
 
-export_settings() {
-  export -p | grep "ZBR" > backup/settings.env
-}
+  export_settings() {
+    export -p | grep "ZBR" > backup/settings.env
+  }
 
+  confirm() {
+    local message=$1
+    local question=$2
+    local isEnabled=$3
+
+    if [[ "$isEnabled" == "1" ]]; then
+      isEnabled="y"
+    fi
+    if [[ "$isEnabled" == "0" ]]; then
+      isEnabled="n"
+    fi
+
+    while true; do
+      if [[ ! -z $message ]]; then
+        echo "$message"
+      fi
+
+      read -p "$question y/n [$isEnabled]:" response
+      if [[ -z $response ]]; then
+        if [[ "$isEnabled" == "y" ]]; then
+          return 1
+        fi
+        if [[ "$isEnabled" == "n" ]]; then
+          return 0
+        fi
+      fi
+
+      if [[ "$response" == "y" || "$response" == "Y" ]]; then
+        return 1
+      fi
+
+      if [[ "$response" == "n" ||  "$response" == "N" ]]; then
+        return 0
+      fi
+
+      echo "Please answer y (yes) or n (no)."
+      echo
+    done
+  }
 
 TARGET_VERSION=1.2
 
@@ -27,11 +66,23 @@ if [[ ! -f jenkins/.disabled ]] ; then
     exit -1
   fi
 
-  # override alldefault jobs by new ones
+  # override all default jobs by new ones
   docker cp jenkins/resources/jobs jenkins-master:/var/jenkins_home/
   if [[ $? -eq 1 ]]; then
     echo "ERROR! Unable to proceed upgrade as jenkins-master container not available!"
     exit -1
+  fi
+
+  # ask about presence of any private Global Pipeline Library. If not - replace by new value.
+  confirm "" "      Do you have custom Global Pipeline Libraries registered? If answer \"n\" they will be overwritten by new Zebrunner-CE?" "n"
+  if [[ $? -eq 0 ]]; then
+    docker cp jenkins/resources/configs/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml jenkins-master:/var/jenkins_home/
+  fi
+
+  # ask about presence of any custom global choice lists. If not - replace by new values.
+  confirm "" "      Do you have custom Global Pipeline Libraries registered? If answer \"n\" they will be overwritten by new Zebrunner-CE?" "n"
+  if [[ $? -eq 0 ]]; then
+    docker cp jenkins/resources/configs/jp.ikedam.jenkins.plugins.extensible_choice_parameter.GlobalTextareaChoiceListProvider.xml jenkins-master:/var/jenkins_home/
   fi
 fi
 
