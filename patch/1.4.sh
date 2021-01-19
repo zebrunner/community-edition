f2b2bcae49bc09d8e38ed330b6f1e4fe21e6b08f
vdelendik <vdelendik@zebrunner.com>
2021-01-19 23:33:48 +0000

    implemented jenkins upgrade steps

--- patch/1.4.sh
+++ patch/1.4.sh
@@ -16,7 +16,26 @@ if ! [[ "${TARGET_VERSION}" > "${SOURCE_VERSION}" ]]; then
 fi
 
 echo "Upgrading Zebrunner from ${SOURCE_VERSION} to ${TARGET_VERSION}"
-echo "TODO: implement upgrade steps!"
+# apply jenkins changes
+if [[ ! -f jenkins/.disabled ]] ; then
+  # remove ./resources/init.groovy.d/setup_aws_credentials.groovy
+  docker run --rm --volumes-from jenkins-master "ubuntu" bash -c "rm /var/jenkins_home/init.groovy.d/setup_aws_credentials.groovy"
+  if [[ $? -ne 0 ]]; then
+    echo "ERROR! Unable to proceed upgrade as jenkins-master container not available!"
+    exit 1
+  fi
+
+  # regenerage variables.env to register new ZEBRUNNER_VERSION var
+  cp jenkins/variables.env.original jenkins/variables.env
+  sed -i "s#http://localhost:8080/jenkins#$ZBR_PROTOCOL://$ZBR_HOSTNAME:$ZBR_PORT/jenkins#g" jenkins/variables.env
+  sed -i "s#INFRA_HOST=localhost:8080#INFRA_HOST=${ZBR_INFRA_HOST}#g" jenkins/variables.env
+
+  if [[ ! -z $ZBR_SONAR_URL ]]; then
+    sed -i "s#SONAR_URL=#SONAR_URL=${ZBR_SONAR_URL}#g" jenkins/variables.env
+  fi
+fi
+
+#TODO: finish upgrade steps for mcloud/reporting/selenoid if necessary
 
 echo "Upgrade to ${TARGET_VERSION} finished successfully"
 
